d60aff65fd49219387257eba143c5952c0c1099c
donggyu <dongco213@naver.com>
2023-06-23 16:30:22 +0900

    test

--- backend/src/main/java/com/kanban/back/BackApplication.java
+++ backend/src/main/java/com/kanban/back/BackApplication.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableEncryptableProperties
 @EnableJpaAuditing
+@EnableScheduling
 public class BackApplication {
 
 	public static void main(String[] args) {
